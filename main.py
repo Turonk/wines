90fd203fbf9598b30d8755f9402bc64ac489350c
Turonk <gorlov.a@mail.ru>
2021-01-27 16:07:56 +0300

    Done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from collections import defaultdict
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from pprint import pprint
+
 
 import datetime
 import pandas
@@ -20,16 +20,15 @@ delta = now - start
 delta = int(delta.days // 365)
 
 # Считывание таблицы вин
-excel_data_df = pandas.read_excel('wine2.xlsx')  # ,usecols=['Название', 'Сорт', 'Цена', 'Картинка'])
+excel_data_df = pandas.read_excel('wine3.xlsx', na_values=['N/A', 'NA'],
+                                  keep_default_na=False)
 
 wines = excel_data_df.to_dict(orient='records')
 groups_wines = defaultdict(list)
 for wine in wines:
     groups_wines[wine['Категория']].append(wine)
 
-#pprint(groups_wines)
 
-#print(groups_wines.keys())
 rendered_page = template.render(groups=groups_wines, years_old=delta)
 
 with open('index.html', 'w', encoding="utf8") as file:
